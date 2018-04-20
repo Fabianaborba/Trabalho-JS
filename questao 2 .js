aec3096f422b2f493e21f0220dc53a7847beb013
Fabianaborba <30900051+Fabianaborba@users.noreply.github.com>
2018-04-20 01:49:38 -0300

    Update questao 2 .js

--- questao 2 .js
+++ questao 2 .js
@@ -1,5 +1,5 @@
-﻿// 2.	 Escreva um programa que solicite o nome do usuário e escreva o Boa noite e o  nome da pessoa.  Exemplo: Qual seu nome: João
-// Boa noite, João. 
+/* 2.screva um programa que solicite o nome do usuário e escreva o Boa noite e o  nome da pessoa.  Exemplo: Qual seu nome:
+Boa noite, João. */
 
 var nome;
 	nome=prompt(" Digite o nome: ");
