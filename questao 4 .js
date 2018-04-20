7dde8dbd44fe1329981efa9f8e0d591c79801d8d
Fabianaborba <30900051+Fabianaborba@users.noreply.github.com>
2018-04-20 19:58:07 -0300

    Update questao 4 .js

--- questao 4 .js
+++ questao 4 .js
@@ -1,12 +1,11 @@
 // 4. Solicitar um número para um usuário e informar se o número é par ou ímpar.
 
 var numero, nome;
-	numero = prompt (" Digite um numero: ");
-	console.log (" Numero: " +nome);
-		if (numero % 2 == 0){
-		console.log ("Numero é par");
-
-		}
-		else{
-		console.log ("Numero é impar");
-		}
+numero = prompt (" Digite um numero: ");
+console.log (" Numero: " +nome);
+	if (numero % 2 == 0){
+	console.log ("Numero é par");
+	}
+	else{
+	console.log ("Numero é impar");
+	}
