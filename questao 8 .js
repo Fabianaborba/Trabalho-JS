536807a0a48081d56725eb50ad651d5d9a61863c
Fabianaborba <30900051+Fabianaborba@users.noreply.github.com>
2018-04-20 19:59:45 -0300

    Update questao 8 .js

--- questao 8 .js
+++ questao 8 .js
@@ -1,9 +1,7 @@
-﻿// 8. Escreva um programa para ler um valor (do teclado) e escrever (na tela) o seu antecessor.  Por exemplo, se o usuário digitar 5 deve aparecer na tela o número 4. 
+// 8. Escreva um programa para ler um valor (do teclado) e escrever (na tela) o seu antecessor.  Por exemplo, se o usuário digitar 5 deve aparecer na tela o número 4. 
 
 var numero;
 
-	numero = Number(prompt ("digite um numero"));
-
-	numero = numero - 1;
-
-	alert (" o antecessor é:" + numero);
+numero = Number(prompt ("digite um numero"));
+numero = numero - 1;
+alert (" o antecessor é:" + numero);
